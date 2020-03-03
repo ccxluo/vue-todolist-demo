a16bcad55634d14534a1a18d4866a8e12e22ed6d
cxluo <cxluo@thoughtworks.com>
2020-03-03 17:59:43 +0800

    remove package element-ui

--- src/main.js
+++ src/main.js
@@ -2,15 +2,7 @@ import Vue from 'vue';
 import App from './App.vue';
 import store from './store';
 
-import {
-  Button,
-  ButtonGroup
-} from 'element-ui';
-
 Vue.config.productionTip = false
-
-Vue.use(Button);
-Vue.use(ButtonGroup);
 
 new Vue({
   render: h => h(App),
